48e7031c2b38210b4d7084fa40fd8f1d054817a4
popoveka <popoveka@fel.cvut.cz>
2022-05-05 11:14:55 +0300

    move implementation fixed

--- src/main/java/popova/simulation/Main.java
+++ src/main/java/popova/simulation/Main.java
@@ -14,12 +14,12 @@ public class Main {
 //        System.out.println(controller.getForestMap());
 
         ForestMap map = new ForestMap();
-        Hare hare1 = new Hare(4,3,4);
+//        Hare hare1 = new Hare(4,3,4);
         Hare hare = new Hare(3,4,3);
-        map.setItem(hare1, new Coordinates(0, 0));
-        map.setItem(hare, new Coordinates(0, 1));
+//        map.setItem(hare1, new Coordinates(0, 5));
+        map.setItem(hare, new Coordinates(8, 7));
 
-        hare1.makeNewAnimal(map, 0, 0);
+        hare.move(map, 8, 7);
         System.out.println(map);
 
 //
--- src/main/java/popova/simulation/interfaces/Moveable.java
+++ src/main/java/popova/simulation/interfaces/Moveable.java
@@ -16,7 +16,7 @@ public abstract class Moveable extends Item {
         for (int pos = x+1; pos <= x+getSpeed(); pos++) {
             Item item;
             try {
-                item = forestMap.getItem(new Coordinates(x, y));
+                item = forestMap.getItem(new Coordinates(pos, y));
             }
             catch (ArrayIndexOutOfBoundsException e) {
                 break;
@@ -27,6 +27,61 @@ public abstract class Moveable extends Item {
             currentX++;
         }
         forestMap.setItem(this, new Coordinates(currentX, y) );
+        forestMap.removeItem(this, new Coordinates(x,y));
+
+        currentX = x;
+
+        for (int pos = x-1; pos >= x-getSpeed(); pos--) {
+            Item item;
+            try {
+                item = forestMap.getItem(new Coordinates(pos, y));
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+            if (item != null) {
+                break;
+            }
+            currentX--;
+        }
+        forestMap.setItem(this, new Coordinates(currentX, y) );
+        forestMap.removeItem(this, new Coordinates(x,y));
+
+        int currentY = y;
+
+        for (int pos = y+1; pos <= y+getSpeed(); pos++) {
+            Item item;
+            try {
+                item = forestMap.getItem(new Coordinates(x, pos));
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+            if (item != null) {
+                break;
+            }
+            currentY++;
+        }
+        forestMap.setItem(this, new Coordinates(x, currentY) );
+        forestMap.removeItem(this, new Coordinates(x,y));
+
+        currentY = y;
+
+        for (int pos = y-1; pos >= y-getSpeed(); pos--) {
+            Item item;
+            try {
+                item = forestMap.getItem(new Coordinates(x, pos));
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+            if (item != null) {
+                break;
+            }
+            currentY--;
+        }
+        forestMap.setItem(this, new Coordinates(x, currentY) );
+        forestMap.removeItem(this, new Coordinates(x,y));
 
     }
 }
